fe17581d326f16fd74fa3a3c125028c406364be7
Felix <Felix@172.18.10.189>
2013-12-05 18:30:21 +0100

    Variables / Getters / Setters

--- src/Modele/Case.java
+++ src/Modele/Case.java
@@ -1,31 +1,53 @@
 package Modele;
-
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class Case {
 
-  private Integer x;
+  private Integer rang;
+  private Integer taille;
+  private ArrayList<Element> listeElements;
+  private ArrayList<Case>  listeCasesAdjacentes;
+//-----------------------------CONSTRUCTEUR--------------------------------------------//
+  public Case(Integer rang,Integer taille,ArrayList<Element> listeElements,ArrayList<Case>  listeCasesAdjacentes){
+	  setRang(rang);
+	  setTaille(taille);
+	  setListeElements(listeElements);
+	  setListeCasesAdjacentes(listeCasesAdjacentes);
+	  
+  }
+//-----------------------------GETTERS AND SETTERS-------------------------------------//
+  public Integer getRang() {
+		return rang;
+	}
 
-  private Integer y;
+	public void setRang(Integer rang) {
+		this.rang = rang;
+	}
 
-  public Integer taille;
+	public Integer getTaille() {
+		return taille;
+	}
 
-    public Vector  myElement1;
- 
- 
-    /**
-   * 
-   * @element-type Element
-   */
-  public Vector  myElement;
-    public Map myMap;
-    /**
-   * 
-   * @element-type Case
-   */
-  public Vector  myCase;
+	public void setTaille(Integer taille) {
+		this.taille = taille;
+	}
 
+	public ArrayList<Element> getListeElements() {
+		return listeElements;
+	}
 
+	public void setListeElements(ArrayList<Element> listeElements) {
+		this.listeElements = listeElements;
+	}
+
+	public ArrayList<Case> getListeCasesAdjacentes() {
+		return listeCasesAdjacentes;
+	}
+
+	public void setListeCasesAdjacentes(ArrayList<Case> listeCasesAdjacentes) {
+		this.listeCasesAdjacentes = listeCasesAdjacentes;
+	}
+//-----------------------------AUTRES METHODES-----------------------------------------//
   public void addObstacle() {
   }
 
