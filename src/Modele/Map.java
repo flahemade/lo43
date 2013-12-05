4f5f155b327ebf9593cc3cdc13aaa71029793824
Felix <Felix@172.18.10.189>
2013-12-05 18:15:20 +0100

    Méthodes basiques écrites.

--- src/Modele/Map.java
+++ src/Modele/Map.java
@@ -8,20 +8,46 @@ public class Map {
 	private String Nom;
     private ParseurXML myParseurXML;
     private Plateau myPlateau;
-    /**
-   * 
-   * @element-type Case
-   */
     private ArrayList<Case>  listeCases;
+//-------------------------------- CONSTRUCTEUR --------------------------------//   
+    public Map(String nom,Plateau plateau,ArrayList<Case> listeCases){
+    	setNom(nom);
+    	setMyPlateau(plateau);
+    	setListeCases(listeCases);
+    }
 
-  public void charger(Map map) {
-  }
-
-  public void enregistrer(Map map) {
-  }
-
-  public Map rafraichir(Map map){
-	  return map;
-  }
+//-------------------------------- SETTERS AND GETTERS --------------------------------//
+	public String getNom() {
+		return Nom;
+	}
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+	public ParseurXML getMyParseurXML() {
+		return myParseurXML;
+	}
+	public void setMyParseurXML(ParseurXML myParseurXML) {
+		this.myParseurXML = myParseurXML;
+	}
+	public Plateau getMyPlateau() {
+		return myPlateau;
+	}
+	public void setMyPlateau(Plateau myPlateau) {
+		this.myPlateau = myPlateau;
+	}
+	public ArrayList<Case> getListeCases() {
+		return listeCases;
+	}
+	public void setListeCases(ArrayList<Case> listeCases) {
+		this.listeCases = listeCases;
+	}
+//---------------------------------------Autres méthodes-------------------------------//
+	  public void charger(Map map) {
+	  }
+	  public void enregistrer(Map map) {
+	  }
+	  public Map rafraichir(Map map){
+		  return map;
+	  }
 
 }
