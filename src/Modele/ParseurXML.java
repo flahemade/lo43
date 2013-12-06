44fad7211f08190848e4a5f3d5575b9934ff5766
Petrol- <nicolas.hahn@unilor.fr>
2013-12-06 01:05:05 +0100

    Created folder /res added 1st version of map.XML started to write the parser constructor

--- src/Modele/ParseurXML.java
+++ src/Modele/ParseurXML.java
@@ -1,15 +1,52 @@
 package Modele;
 
 
+import javax.xml.parsers.*; 
+
+import org.w3c.dom.*; 
+import org.xml.sax.*; 
+
+import java.io.*; 
+
+
+/**
+ * This class is used to load the map and its content (obstacles and species) described
+ * in the XML file
+ *  
+ * @author nicolas
+ *
+ */
+
 public class ParseurXML{
 
-/** Definition des attributs */
+/** -----attributes definitions-----*/
+	
   private String path; /* Chemin relatif vers le fichier XML*/
 
-
+/** 
+ * Initializes the parser using String Chemin 
+ * to get to the file to parse
+ * @param chemin
+ */
   public ParseurXML(String chemin){
-  
+	  try {
+	  
+	  DocumentBuilderFactory fabrique = DocumentBuilderFactory.newInstance(); //
+	  DocumentBuilder constructeur = fabrique.newDocumentBuilder();
+	  File xml = new File(chemin);
+	  Document document = constructeur.parse(xml);
+	  
+	  }catch(Exception e){
+		  //TODO Gerer l'exception
+	  }
+	  
   }
+	  
+	  
+  
+  
+  
+/**-------- Getters & setters ------*/
   
   public String getPath() {
 	return path;
@@ -19,16 +56,19 @@ public void setPath(String path) {
 	this.path = path;
 }
 
+/**------------ Methods ------------*/
 
+/*
 public void write(String chemin, Map map) {
   }
 
   public Map read(String chemin) {
-	  Map map = new Map();
-	  return map;
+	  	
+	  
   }
 
   public void save(String chemin, Map map ) {
   }
-
+*/
 }
+
