e9b6dc24e94815a18d307f930a20de32e05af610
AnaelleI <anaelle.isola@aliceadsl.fr>
2013-12-08 14:10:50 +0100

    Amélioration de la fenêtre principale

--- src/Vue/FenetreAccueil.java
+++ src/Vue/FenetreAccueil.java
@@ -35,7 +35,7 @@ public class FenetreAccueil extends JFrame{
 	{
 		super(titre);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(new Dimension(700,500));
+		this.setSize(new Dimension(500,300));
 		creerComposants();
 		placerComposants();
 	}
@@ -47,7 +47,9 @@ public class FenetreAccueil extends JFrame{
 	public void creerComposants() 
 	  {
 		  btn_jouer = new JButton("Jouer");
+		  btn_jouer.setPreferredSize(new Dimension(100,30));
 		  messageAccueil = new JLabel("Bienvenue sur Savane Yolo !!!");
+		  
 	  }
 	
 	  /*_______________________________________________________________*/
@@ -57,10 +59,11 @@ public class FenetreAccueil extends JFrame{
 	{
 		JPanel jp1 = new JPanel();
 		jp1.setLayout(new GridLayout(3, 1));
+		
 		jp1.add(btn_jouer);
 		this.getContentPane().setLayout(new BorderLayout());
 		this.getContentPane().add(jp1, BorderLayout.EAST);
-		this.getContentPane().add(messageAccueil, BorderLayout.NORTH);
+		this.getContentPane().add(messageAccueil, BorderLayout.CENTER);
 	}
 	  
 	  /*_______________________________________________________________*/
