1484eeacbd1a28a08e149d3e604dfb46c6d9da1e
Petrol- <nicolas.hahn@unilor.fr>
2013-12-08 18:43:46 +0100

    Added class Position

---
+++ src/Modele/Position.java
@@ -0,0 +1,36 @@
+package Modele;
+/**
+ * Class permettant de représenter 
+ * simplement une position
+ * @author nicolas
+ *
+ */
+public class Position { 
+	private int x;
+	private int y;
+	
+	public Position(){
+		x = 0;
+		y = 0;
+	}
+	/* Constructor */
+	public Position(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	/* getters & Setters */
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
