2ecf859db30c7a42efaf51d31c753de366619120
Felix <Felix@172.18.16.214>
2013-12-09 13:26:13 +0100

    Ordonnanceur : Constructeur, getters et setters.

--- src/Controleur/Ordonnanceur.java
+++ src/Controleur/Ordonnanceur.java
@@ -1,5 +1,4 @@
 package Controleur;
-import java.util.Vector;
 
 /*_______________________________________________________________*/
 /**
@@ -7,13 +6,26 @@ import java.util.Vector;
  *
  */
 public class Ordonnanceur {
-
-  public Integer newAttr;
-
-    public Vector  lance;
-    public Vector  rafraichir;
-
-  public void run() {
-  }
+	private Boolean pause;
+	//-------------------------------------------CONSTRUCETEUR---------------------------//
+	public Ordonnanceur(){
+		setPause(true);
+	}
+	//-------------------------------------------GETTERS AND SETTERS---------------------//
+	public Boolean getPause() {
+		return pause;
+	}
+	public void setPause(Boolean pause) {
+		this.pause = pause;
+	}
+	//-------------------------------------------AUTRES METHODES-------------------------//
+	public void run() {
+		  while(this.pause==false){
+			  // Jouer...
+		  }
+	}
+	public void stop(){
+		  setPause(true);
+	}
 
 }
